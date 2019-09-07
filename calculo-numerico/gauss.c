7865424249908d47719413622062c627ca855cba
Lenardo Costa <leo.conectividade@gmail.com>
2019-09-06 22:11:41 -0300

    adição da função sretro que resolve um SL TS

--- calculo-numerico/gauss.c
+++ calculo-numerico/gauss.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define EPSILON 0.000001
 
 /*Se houver memória disponível, aloca uma matriz bidimencional de double com l linhas e c colunas
  e devolve um ponteiro para essa matriz; Caso contrário, devolve um ponteiro nulo*/
@@ -9,22 +11,37 @@ double **aloca_matriz(int l, int c);
 void le_matriz(double **m, int l, int c);
 
 /*imprime uma matriz de double com l linhas e c colunas*/
-void imprime_matriz(double **m, int l, int c);	
+void imprime_matriz(double **m, int l, int c);
+
+/*Recebe m, a matriz reduzida de um sl ts com n varapaveis. se o sl for determinado, coloca com x a solução do sl e devolve 0;
+ se for determinado, coloca em x uma dsolução e devolve 1; se for incompativel=, devolve 2*/
+ int sretro(double **m, int n, double x[]);
 
 int main(){
 	
-	int n;
-	double **A;
+	int n, i, tipo;
+	double **A, *x;
 	printf("Quantidade de variáveis: ");
 	scanf("%d", &n);
 	A = aloca_matriz(n, n+1);
-	if(A == NULL) /*falta de memória*/	
+	x = malloc(sizeof(double)*n);
+	if(A == NULL || x == NULL) /*falta de memória*/	
 	{
 		printf("Deu pau\nFalta de memória");
 		return 1;
 	}
 	le_matriz(A, n, n+1);
 	imprime_matriz(A, n, n+1);
+	tipo = sretro(A, n, x);
+	if(tipo == 2){
+		printf("LS INCOMPATÍVEL");
+	}else{
+		printf("SL %sDETERMINADO\n", tipo?"IN":"");
+		for(i=0; i<n; i++){
+			printf("x[%d]: %10.2lf\n", i+1, x[i]);
+		}
+	}
+	
 	return 0;
 }/*fim do main*/
 
@@ -66,3 +83,25 @@ void imprime_matriz(double **m, int l, int c){
 		printf("\n");
 	}
 }
+
+int sretro(double **m, int n, double x[]){
+	int i, j, tipo = 0;
+	double soma;
+	for(i=n-1; i>=0; i--){
+		soma = 0;
+		for (j=i+1; j<n; j++){
+			soma += m[i][j]*x[j];
+		}
+		if(m[i][i] == 0){
+			if( fabs(m[i][n]-soma) < EPSILON){  /*xi é variável livre*/
+				tipo = 1;
+				x[i] = 0;
+			}
+			else {	return 2; }							
+		}
+		else{
+			x[i] = (m[i][n] - soma)/m[i][i];
+		}
+	}
+	return tipo;
+}/*fim do sretro*/
